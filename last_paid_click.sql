58bce5d0b96ca36860eaed3324e34ee679da84a7
artem alekseev <temchic1@gmail.com>
2024-03-03 15:45:48 +0300

    Update last_paid_click files again

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,36 +1,40 @@
+with tab as (
+    select
+        s.visitor_id,
+        s.visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        row_number()
+        over (
+            partition by s.visitor_id
+            order by s.visit_date desc
+        )
+        as visit_rank
+    from sessions as s
+    where s.medium != 'organic'
+)
 select
-	distinct s.visitor_id,
-	MAX(s.visit_date) as visit_date,
-	s.source as utm_source,
-	s.medium as utm_medium,
-	s.campaign as utm_campaign,
-	l.lead_id,
-	l.created_at,
-	SUM(l.amount) as amount,
-	l.closing_reason,
-	l.status_id
-from sessions as s
+    t.visitor_id,
+    t.visit_date as visit_date,
+    t.utm_source,
+    t.utm_medium,
+    t.utm_campaign,
+    l.lead_id,
+    l.created_at as created_at,
+    l.amount,
+    l.closing_reason,
+    l.status_id
+from tab as t
 left join leads as l
-	using (visitor_id)
-where
-	s.medium = 'cpc'
-	or s.medium = 'cpm'
-	or s.medium = 'cpa'
-	or s.medium = 'youtube'
-	or s.medium = 'cpp'
-	or s.medium = 'tg'
-	or s.medium = 'social'
-group by
-	s.visitor_id,
-	s.source,
-	s.medium,
-	s.campaign,
-	l.lead_id,
-	l.created_at,
-	l.closing_reason,
-	l.status_id
+    on
+        t.visitor_id = l.visitor_id
+        and t.visit_date <= l.created_at
+where t.visit_rank = 1
 order by
-	amount desc nulls last,
-	visit_date asc,
-	utm_source
+    l.amount desc nulls last,
+    t.visit_date asc,
+    t.utm_source asc,
+    t.utm_medium asc,
+    t.utm_campaign asc
 limit 10;
