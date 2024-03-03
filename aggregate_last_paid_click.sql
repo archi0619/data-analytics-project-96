9bec010de3947afa2ce271fd3e4a8d8b8e8d1ff1
artem alekseev <temchic1@gmail.com>
2024-03-03 17:25:58 +0300

    Correction aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,74 +1,74 @@
-with tab1 as
-(
-    select distinct on (s.visitor_id) s.visitor_id,
-        s.visit_date,
-    	l.created_at,
-    	l.status_id,
+with tab1 as (
+    select
+    	distinct on (s.visitor_id) s.visitor_id,
+    	s.visit_date,
+        l.created_at,
+        l.status_id,
     	l.amount,
     	l.lead_id,
     	l.closing_reason,
     	s.medium,
     	s.source,
-        s.campaign
-   from sessions as s
-   left join leads as l
-        on s.visitor_id = l.visitor_id
-        and s.visit_date <= l.created_at
-   where s.medium != 'organic'
-   order by
-        s.visitor_id asc,
-        s.visit_date desc
-),
-tab as
-(
-   select
-        va.utm_source,
-        va.utm_medium,
-        va.utm_campaign,
-        cast(va.campaign_date as date) as campaign_date,
-        sum(va.daily_spent) as total_cost
-   from vk_ads as va
-   group by
-        va.utm_source,
-        va.utm_medium,
-        va.utm_campaign,
-        cast(va.campaign_date as date)
-   union
-   select
-        ya.utm_source,
-        ya.utm_medium,
-        ya.utm_campaign,
-        cast(ya.campaign_date as date) as campaign_date,
-        sum(ya.daily_spent) as total_cost
-   from ya_ads as ya
-   group by
-        ya.utm_source,
-        ya.utm_medium,
-        ya.utm_campaign,
-        cast(ya.campaign_date as date)
-),
-tab2 as
-(
-   select
-        tab1.source,
-        tab1.medium,
-        tab1.campaign,
-        cast(tab1.visit_date as date) as visit_date,
-        count(tab1.visitor_id) as visitors_count,
-        count(tab1.visitor_id) filter (
-     where
-        tab1.created_at is not null ) as leads_count,
-        count(tab1.visitor_id) filter (
-     where tab1.status_id = 142 ) as purchases_count,
-        sum(tab1.amount) filter (
-     where tab1.status_id = 142) as revenue
-   from
+    	s.campaign
+    from sessions as s
+    left join leads as l
+         on s.visitor_id = l.visitor_id
+         and s.visit_date <= l.created_at
+    where s.medium != 'organic'
+    order by
+         s.visitor_id asc,
+         s.visit_date desc
+)
+,
+tab as (
+    select
+         va.utm_source,
+         va.utm_medium,
+         va.utm_campaign,
+         cast(va.campaign_date as date) as campaign_date,
+         sum(va.daily_spent) as total_cost
+    from vk_ads as va
+    group by
+         va.utm_source,
+         va.utm_medium,
+         va.utm_campaign,
+         cast(va.campaign_date as date)
+    union
+    select
+         ya.utm_source,
+         ya.utm_medium,
+         ya.utm_campaign,
+         cast(ya.campaign_date as date) as campaign_date,
+         sum(ya.daily_spent) as total_cost
+    from ya_ads as ya
+    group by
+         ya.utm_source,
+         ya.utm_medium,
+         ya.utm_campaign,
+         cast(ya.campaign_date as date)
+)
+,
+tab2 as (
+    select
+         tab1.source,
+         tab1.medium,
+         tab1.campaign,
+         cast(tab1.visit_date as date) as visit_date,
+         count(tab1.visitor_id) as visitors_count,
+         count(tab1.visitor_id) filter (
+      where
+         tab1.created_at is not null ) as leads_count,
+         count(tab1.visitor_id) filter (
+      where tab1.status_id = 142 ) as purchases_count,
+         sum(tab1.amount) filter (
+      where tab1.status_id = 142) as revenue
+    from
         tab1
-   group by
-        tab1.source,
-        tab1.medium,
-        tab1.campaign,
-        cast(tab1.visit_date as date)
+    group by
+         tab1.source,
+         tab1.medium,
+         tab1.campaign,
+         cast(tab1.visit_date as date)
 )
 select
     to_char(tab2.visit_date, 'yyyy-mm-dd') as visit_date,
